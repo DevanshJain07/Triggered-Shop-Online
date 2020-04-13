b63dd66f5ebe741febc5e0ffadf47e75cec2d0da
devansh071999 <devanshsampatjain@gmail.com>
2020-04-13 13:40:52 +0530

    category updation can be done

--- projbackend/routes/category.js
+++ projbackend/routes/category.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const {getCategoryById,createCategory,getCategory,getAllCategory} = require("../controllers/category");
+const {getCategoryById,createCategory,getCategory,getAllCategory,updateCategory} = require("../controllers/category");
 const {getUserById} = require("../controllers/user");
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 
@@ -10,9 +10,16 @@ router.param("userId", getUserById);
 router.param("userId", getCategoryById);
 
 // actual routers goes here
+
+//CREATE ROUTES
 router.post("category/create/:userId",isSignedIn, isAuthenticated, isAdmin,createCategory);
+
+//READ ROUTES
 router.get("/category/:categoryId",getCategory);
 router.get("/category/:categories",getAllCategory);
 
+//UPDATE
+router.post("category/:categoryId/:userId",isSignedIn, isAuthenticated, isAdmin,updateCategory);
 
+//DELETE
 module.exports = router;
