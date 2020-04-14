30b4694d8ea8effae8016340021a2726bc834f35
devansh071999 <devanshsampatjain@gmail.com>
2020-04-14 18:50:43 +0530

    updation can be done

--- projbackend/controllers/product.js
+++ projbackend/controllers/product.js
@@ -89,5 +89,40 @@ exports.deleteProduct=(req,res)=>{
 };
 
 exports.updateProduct=(req,res)=>{
-  
+  let form=new formidable.IncomingForm();
+    form.keepExtensions=true;
+
+    form.parse(req,(err,fields,file)=>{
+        if (err) {
+            return res.status(400).json({
+              error: "Problem with image"
+            });
+          }
+          //updation code
+          let product=req.product;
+          product=_.extend(product,fields)
+          
+
+          //handle file here
+          if(file.photo){
+              if(file.photo.size>3000000){
+                  return res.status(400).json({
+                      error:"File size is too big!"
+                  })
+              }
+              product.photo.data=fs.readFileSync(file.photo.path);
+              product.photo.contentType=file.photo.type;
+          }
+
+          //save to the DB
+          product.save((err,product)=>{
+            if (err) {
+                return res.status(400).json({
+                  error: "Updation of product failed"
+                });
+              }
+              res.json(product);
+          });
+    });
 };
+
