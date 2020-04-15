a0db2d11e6a825f0255c03c2d6d0b85cfd3c5719
devansh071999 <devanshsampatjain@gmail.com>
2020-04-15 13:31:14 +0530

    getting of all the orders done

--- projbackend/routes/order.js
+++ projbackend/routes/order.js
@@ -3,12 +3,16 @@ const router = express.Router();
 const {getUserById,pushOrderInPurchaseList} = require("../controllers/user");
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 const{updateStock}=require("../controllers/product");
-const {getOrderById}=require("../controllers/order");
+const {getOrderById,createOrder,getAllOrders}=require("../controllers/order");
 
 //params
 router.param("userId",getUserById);
 router.param("orderId",getOrderById);
 
 //Actual routes
+//create
+router.post("/order/create/:userId",isSignedIn, isAuthenticated,pushOrderInPurchaseList,updateStock,createOrder);
+//read
+router.get("/order/all/:userId",isSignedIn, isAuthenticated,isAdmin,getAllOrders);
 
 module.exports=router;
