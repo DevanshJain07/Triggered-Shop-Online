57e696302e876e16b295653dbae14a60e2ae0800
devansh071999 <devanshsampatjain@gmail.com>
2020-04-17 20:55:12 +0530

    BACKEND CONNECTED TO FRONTEND

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "../styles.css";
-
+import {API} from "../backend";
 
 export default function Home(){
+    console.log("API IS",API);
+
   return(
     <div>
       <h1 className="text-white">Hello here i start my frontend development.</h1>
