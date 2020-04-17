d355bff120936dd0f8c716bfc50a5dbf64eb5fc9
devansh071999 <devanshsampatjain@gmail.com>
2020-04-17 20:15:16 +0530

    style added

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import "../styles.css";
+
 
 export default function Home(){
   return(
     <div>
-      <h1>Hello here i start my frontend development.</h1>
+      <h1 className="text-white">Hello here i start my frontend development.</h1>
     </div>
   );
 }
