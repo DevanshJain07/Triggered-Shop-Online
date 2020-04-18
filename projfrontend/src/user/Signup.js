7feb7af5cc00bc8bec5e3ad627da73c668a7e0e5
devansh071999 <devanshsampatjain@gmail.com>
2020-04-19 00:02:47 +0530

    signup completed

--- projfrontend/src/user/Signup.js
+++ projfrontend/src/user/Signup.js
@@ -49,26 +49,61 @@ const Signup=()=>{
                             <input 
                             className="form-control" 
                             onChange={handleChange("name")}
-                            type="text"/>
+                            type="text"
+                            value={name}
+                            />
                         </div>
                         <div className="form-group">
                             <label className="text-light">Email</label>
-                            <input className="form-control" onChange={handleChange("email")} type="email"/>
+                            <input value={email} className="form-control" onChange={handleChange("email")} type="email"/>
                         </div>
                         <div className="form-group">
                             <label className="text-light">Password</label>
-                            <input className="form-control" onChange={handleChange("password")} type="password"/>
-                        </div>
-                        <button className="btn btn-success btn-block">Submit</button>
+                            <input value={password} className="form-control" onChange={handleChange("password")} type="password"/>
+                        </div>  
+                        <button onClick={onSubmit} className="btn btn-success btn-block">Submit</button>
                     </form>
                 </div>
             </div>
         );
     };
 
+    const successMessage=()=>{
+        return(
+         <div className="row">
+         <div className="col-md-6 offset-md-3 text-left">
+                   
+        <div className="alert alert-success" 
+        style={{display:success ? "":"none"}}>
+            New account was created successfully. Please{" "} 
+            <Link to="/signin">Login Here</Link>
+        </div>
+        </div>
+        </div>
+
+        );
+    };
+
+    const errorMessage=()=>{
+        return(
+         <div className="row">
+         <div className="col-md-6 offset-md-3 text-left">
+                     
+        <div className="alert alert-danger" 
+        style={{display:error ? "":"none"}}>
+            {error}
+        </div>
+        </div>
+        </div>
+
+        );
+    };
+    
 
     return(
         <Base title="Sign Up Page" description="A page for user to sign up!">
+            {successMessage()}
+            {errorMessage()} 
             {signUpForm()}
             <p className="text-white text-center">{JSON.stringify(values)}</p>
         </Base>
