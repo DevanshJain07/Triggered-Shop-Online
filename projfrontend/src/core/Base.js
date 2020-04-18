db16be8795ef9cd8804aa5298528afef4a2d33ef
devansh071999 <devanshsampatjain@gmail.com>
2020-04-18 13:28:32 +0530

    styling added to know the present opened up tab

--- projfrontend/src/core/Base.js
+++ projfrontend/src/core/Base.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Menu from './Menu';
 
 const Base=({
     title="My Title",
@@ -7,12 +8,13 @@ const Base=({
     children
 })=>(
     <div>
+        <Menu/>
         <div className=".container-fluid">
             <div className="jumbotron bg-dark text-white text-center">
                 <h2 className="display-4">{title}</h2>
                 <p className="lead">{description}</p>
             </div>
-            <div className={className}>{children}></div>
+            <div className={className}>{children}</div>
         </div>
         <footer className="footer bg-dark mt-auto py-3">
             <div className="container-fluid bg-success text-white text-center py-3">
