7b551b83867339bcabe54030b398208396d26eaa
devansh071999 <devanshsampatjain@gmail.com>
2020-04-20 17:54:00 +0530

    categories can be added

--- projfrontend/src/admin/AddCategory.js
+++ projfrontend/src/admin/AddCategory.js
@@ -2,11 +2,12 @@ import React,{useState} from "react"
 import Base from "../core/Base";
 import { isAuthenticated } from "../auth/helper";
 import { Link } from "react-router-dom";
+import {createCategory} from "./helper/adminapicall";
 
 
 const AddCategory=()=>{
 
-    const [name,setName]=useState("initialState");
+    const [name,setName]=useState("");
     const [error,setError]=useState(false);
     const [success,setSuccess]=useState(false);
 
@@ -18,12 +19,34 @@ const AddCategory=()=>{
         </div>
     );
 
+    const handleChange=event=>{
+        setError("");
+        setName(event.target.value);
+    };
+
+    const onSubmit=(event)=>{
+        event.preventDefault();
+        setError("");
+        setSuccess(false);
+
+        createCategory(user._id,token,{name})
+        .then(data=>{
+            if (data.error){
+                setError(true);
+            }else{
+                setError("");
+                setSuccess(true);
+            }
+        });
+    };
+
+
     const myCategoryForm=()=>(
         <form>
             <div className="form-group">
                 <p className="lead">Enter the category</p>
-                <input type="text" className="form-control my-3" autoFocus required placeholder="For Ex. Summer" />
-                <button className="btn btn-outline-info">Create Category</button>
+                <input onChange={handleChange} value={name} type="text" className="form-control my-3" autoFocus required placeholder="For Ex. Summer" />
+                <button onClick={onSubmit} className="btn btn-outline-info">Create Category</button>
             </div>
         </form>
 
