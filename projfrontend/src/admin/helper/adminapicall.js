54a3acd9a5eb568c7392d383eb5fb4fbbbee2696
devansh071999 <devanshsampatjain@gmail.com>
2020-04-20 19:06:11 +0530

    product calling done

--- projfrontend/src/admin/helper/adminapicall.js
+++ projfrontend/src/admin/helper/adminapicall.js
@@ -13,3 +13,17 @@ export const createCategory=(userId,token,category)=>{
         return response.json();
     }).catch(err => console.log(err));
 };
+
+export const createProduct=(userId,token,product)=>{
+    return fetch(`${API}/product/create/${userId}`,{
+        method:"POST",
+        headers:{
+            Accept:"application/json",
+            Authorization:`Bearer ${token}`
+        },
+        body:product
+    }).then(response=>{
+        return response.json()
+    })
+    .catch(err=>console.log(err))
+}
