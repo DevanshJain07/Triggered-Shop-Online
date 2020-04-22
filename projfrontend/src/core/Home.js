1b303ce4dd1663175e188970119f9d823f4bd36b
devansh071999 <devanshsampatjain@gmail.com>
2020-04-23 01:14:57 +0530

    bugs removed and removeing done from the cart

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,45 +1,42 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "../styles.css";
-import {API} from "../backend";
+import { API } from "../backend";
 import Base from "./Base";
-import Card from "./card";
+import Card from "./Card";
 import { getProducts } from "./helper/coreapicalls";
 
+export default function Home() {
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(false);
 
-export default function Home(){
-
-  const [products,setProducts]=useState([])
-  const [error,setError]=useState(false)
-
-  const loadAllProduct=()=>{
-    getProducts().then(data=>{
-      if(data.error){
-        setError(data.error)
-      }else{
+  const loadAllProduct = () => {
+    getProducts().then(data => {
+      if (data.error) {
+        setError(data.error);
+      } else {
         setProducts(data);
       }
     });
   };
 
-  useEffect(()=>{
-    loadAllProduct()
-  },[])
+  useEffect(() => {
+    loadAllProduct();
+  }, []);
 
-
-  return(
-    <Base title="Home Page" description="Triggered Shop Online">
+  return (
+    <Base title="Home Page" description="Welcome to the Tshirt Store">
       <div className="row text-center">
-        <h1 className="text-white">All of TShirts</h1>
+        <h1 className="text-white">All of tshirts</h1>
         <div className="row">
-          {products.map((product,index)=>{
-            return(
+          {products.map((product, index) => {
+            return (
               <div key={index} className="col-4 mb-4">
-                <Card product={product}/>
-                </div>
-            )
+                <Card product={product} />
+              </div>
+            );
           })}
         </div>
-        </div>
+      </div>
     </Base>
   );
 }
