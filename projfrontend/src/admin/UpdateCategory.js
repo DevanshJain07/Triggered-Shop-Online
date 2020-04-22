6229de0864ddbc92693d1daaf8b85cad37261724
devansh071999 <devanshsampatjain@gmail.com>
2020-04-22 17:46:32 +0530

    debugging

--- projfrontend/src/admin/UpdateCategory.js
+++ projfrontend/src/admin/UpdateCategory.js
@@ -1,74 +1,80 @@
-import React, { useState, useEffect } from "react"
-import Base from "../core/Base"
-import { isAuthenticated } from "../auth/helper"
-import { Link } from "react-router-dom"
-import { updateCategory, getCategory } from "./helper/adminapicall"
+import React, { useState , useEffect} from 'react';
+import Base from "../core/Base";
+import {isAuthenticated} from "../auth/helper";
+import {Link} from "react-router-dom";
+import {updateCategory,getCategory} from "./helper/adminapicall"
 
-const UpdateCategory = ({match}) => {
+const UpdateCategory = ({ match }) => {
 
+    const {user, token} = isAuthenticated();
     const [values, setValues] = useState({
         name: "",
+        loading: false,
         error: "",
-        success: ""
-    })
+        createdCategory: "",
+        getaRedirect: false,
+        formData: "",
+        success:""
+      });
     
-    const {name, error, success} = values
-    const {user, token} = isAuthenticated()
-
-    const goBack = () => (
-        <div className="mt-5">
-            <Link className="btn btn-sm btn-success mb-3" to="/admin/dashboard">Admin Home</Link>
-        </div>
-    )
-
-    const preload = (categoryId) => {
+      const {
+        name,  
+        loading,
+        error,
+        createdCategory,
+        getaRedirect,
+        formData,
+        success
+      } = values;
+    
+      const preload = categoryId => {
         getCategory(categoryId).then(data => {
-            if(data.err){
-                console.log(data.err)
-            } else{
-                setValues({
-                    ...values,
-                    name: data.name
-                })
-            }
-        })
-    }
+          //console.log(data);
+          if (data?.error) {
+            setValues({ ...values, error: data.error });
+          } else {
+            setValues({
+              ...values,
+              name: data.name,
+              formData: new FormData()
+            });
+          }
+        });
+      };
 
-    useEffect(() => {
+      useEffect(() => {
         preload(match.params.categoryId);
-    }, [])
+      }, []);
 
-    const handleChange = name => event => {
-        setValues({...values, error:false, [name]: event.target.value})
-    }
-
-    const onSubmit = (event) => {
-        console.log(name)
+      const onSubmit = event => {
         event.preventDefault();
-        setValues({...values, error: false, success: false})
-
-        //backend request fired
-        updateCategory(match.params.categoryId, user._id, token, name )
-        .then(data => {
-            
-            if(data.error){
-                setValues({...values, error: data.error})
-            } else{
-                setValues({
-                    ...values, 
-                    error: false,
-                    success: true,
-                    name: ""
-                })
+        setValues({ ...values, error: "", loading: true,success:false });
+    
+        updateCategory(match.params.categoryId, user._id, token, formData).then(
+          data => {
+            if (data?.error) {
+              setValues({ ...values, error: data.error });
+            } else {
+              setValues({
+                ...values,
+                name: "",
+                error:false,
+                success:true,
+                loading: false,
+                createdCategory: data.name
+              });
             }
-        })
-    }
+          }
+        );
+      };
+    
+      const handleChange = name => event => {
+        setValues({...values, error:false, [name]: event.target.value});
 
- 
-
-
-
-    const successMessage = () => {
+        //setValues({ ...values });
+      };
+    
+      const successMessage = () => {
         if(success){
             return <h4 className="text-success">Category Updated successfully</h4>
         }
@@ -80,40 +86,44 @@ const UpdateCategory = ({match}) => {
         }
     }
 
-    const myCategoryForm = () => (
+    const goBack = () => (
+        <div className="mt-5">
+            <Link className="btn btn-sm btn-success mb-3" to="/admin/dashboard">Admin Home</Link>
+        </div>
+    );
+
+  
+
+    const myCategoryFrom = () => (
         <form>
             <div className="form-group">
-                <p className="lead">Update the category</p>
-                <input type="text" 
-                className="form-control my-3"
-                name="name"
-                onChange={handleChange("name")}
-                value={name}
-                autoFocus
-                required
-                />
-                <p className="text-center">{JSON.stringify(values)}</p>
-                <button type="submit" onClick={onSubmit} className="btn btn-outline-info">Update Category</button>
+                <p className="lead">Enter the category</p>
+                <div className="form-group">
+        <input
+          onChange={handleChange("name")}
+          name="photo"
+          className="form-control"
+          placeholder="Name"
+          value={name}
+        />
+      </div>
+                <button onClick={onSubmit} className="btn btn-outline-info">update Category</button>
             </div>
         </form>
-    )
+    );
 
-    return(
-        <Base 
-        title="Crete a category here" 
-        description="Add a new category for new tshirts"
-        className="container bg-info p-4"
-        >
-            <div className="row bg-white rounded">
-                <div className="col-md-8 offset-md-2">
+    return (
+        <Base  title="Create a category here" description="Add a new category for new tshirts" className="container bg-info p-4">
+          <div className="row bg-white rounded">
+              <div className="col-md-8 offset-md-2">
                     {successMessage()}
                     {warningMessage()}
-                    {myCategoryForm()}
+                    {myCategoryFrom()}
                     {goBack()}
-                </div>
-            </div>
+              </div>
+          </div>  
         </Base>
-    )
-}
+    );
+};
 
 export default UpdateCategory;
