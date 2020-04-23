f5fb1819a6695ee8a91017aa24dcaa01e95b1809
devansh071999 <devanshsampatjain@gmail.com>
2020-04-23 12:26:56 +0530

    payment braintree

---
+++ projbackend/routes/paymentBRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const { isSignedIn, isAuthenticated} = require("../controllers/auth");
+
+const {getToken,processPayment}=require("../controllers/paymentBRoutes");
+
+router.get("/payment/getToken/:userId",isSignedIn,isAuthenticated,getToken);
+
+router.post("/payment/braintree/:userId",isSignedIn,isAuthenticated,processPayment);
+module.exports=router;
